5d3a44b814460f15326c880b1ca28d3d64155a8a
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-06-29 18:38:18 +0530

    add cardlist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -19,12 +19,8 @@ class App extends Component {
     this.setState(
       () => {
         return {monsters:users}
-      },
-      () => {
-        console.log(this.state)
-      }
+      })
     )
-  )
 }
   onSearchChange = (event) => {
     const searchField = event.target.value.toLocaleLowerCase();
@@ -44,14 +40,7 @@ class App extends Component {
       <div className="App">
         <input className='search-box' type='search' placeholder='Search' onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
-          return (
-          <div key={monster.id}> 
-          <h1>{monster.name}</h1>
-          </div>
-          )
-        })}
-    
+     <CardList monsters={filteredMonsters} />
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        const {monsters} = this.props;
+        return (
+            <div>
+                {monsters.map((monster) => (
+                <div key={monster.id}><h1 >{monster.name}</h1> </div>
+                    )
+                    )}
+            </div>
+        )
+    }
+}
+
+export default CardList;
+
+// {filteredMonsters.map((monster) => {
+//     return (
+//     <div key={monster.id}> 
+//     <h1>{monster.name}</h1>
+//     </div>
+//     )
+//   })}
