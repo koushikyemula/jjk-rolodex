ec67edb3f44c5cbac7c932e436cf988bec6e0027
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-06-29 15:32:36 +0530

    add search filtering

--- src/App.js
+++ src/App.js
@@ -29,8 +29,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='Search' onChange={(event) => {
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          })
+          this.setState(() => {
+            return {monsters: filteredMonsters};
+          })
+        }}
+        />
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>
+          return (
+          <div key={monster.id}> 
+          <h1>{monster.name}</h1>
+          </div>
+          )
         })}
     
       </div>
