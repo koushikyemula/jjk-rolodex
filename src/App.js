cb0c1a826b950f1d5615d3a28a20a50d9df3d682
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-02 20:11:06 +0530

    add cardlist and styling

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className='title-name'> JJK Rolodex </h1>
         <SearchBox className="search-box" placeholder="Search" onChangeHandler={onSearchChange} />
      <CardList monsters={filteredMonsters} />
       </div>
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,16 @@
 import { Component } from "react";
+import "./card-list.styles.css";
+import Card from "../card/card.component";
 
 class CardList extends Component {
     render() {
         const {monsters} = this.props;
         return (
-            <div>
-                {monsters.map((monster) => (
-                <div key={monster.id}><h1 >{monster.name}</h1> </div>
-                    )
-                    )}
+            <div className="card-list">
+                {monsters.map((monster) => {
+                    return ( 
+                        <Card monster = {monster} />
+                )})}
             </div>
         )
     }
