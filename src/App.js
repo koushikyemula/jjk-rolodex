144e73155fcf5767bc822838856101ebe30ab1e0
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-06 23:36:48 +0530

    useeffect and cardlist updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
-  const [searchField , setSearchField] = useState("");
-  console.log(searchField)
+  const [searchField, setSearchField] = useState(""); // [value, setValue]
+  const [monsters, setMonsters] = useState([]);
 
+  useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, []);
+  
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   }
+  
+  const filteredMonsters = monsters.filter((monster) => {
+          return monster.name.toLocaleLowerCase().includes(searchField);
+        })
 
   return (
     <div className="App">
         <h1 className='title-name'> JJK Rolodex </h1>
         <SearchBox className="search-box" placeholder="Search" onChangeHandler={onSearchChange} />
-     {/* <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} /> 
       </div>
   )
 }
